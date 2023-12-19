130c09a34a2ed436eff551518cd1de1ab84775d7
Ryunosuke Konaka <k.ryunosuke257@gmail.com>
2023-12-19 17:24:14 +0900

    Login処理修正

--- AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/AttendanceListController.java
+++ AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/AttendanceListController.java
@@ -1,42 +1,9 @@
 package com.example.AttendanceManage;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.List;
-import java.util.Map;
 
 
 @Controller
 public class AttendanceListController {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @GetMapping("/attendanceList")
-
-    public String index(Model model) {
-        return "attendancelist";
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password, Model model) {
-        String sql = "SELECT * FROM USERS WHERE username = ? AND password = ?";
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, username, password);
-
-        if (result.isEmpty()) {
-            // ログイン失敗時の処理
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        } else {
-            // ログイン成功時の処理
-            return "redirect:/attendanceList";
-        }
-    }
+	
 }
--- AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
+++ AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
@@ -3,34 +3,26 @@ package com.example.AttendanceManage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.AttendanceManage.form.LoginForm;
 
 @Controller
+@RequestMapping("/Login")
 public class LoginController {
-
-        @Autowired
-        private JdbcTemplate jdbcTemplate;
-
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password, Model model) {
-        String sql = "SELECT * FROM USERS WHERE username = ? AND password = ?";
-        List<Map<String, Object>> users = jdbcTemplate.queryForList(sql, username, password);
-
-        if (users.isEmpty()) {
-            // ログイン失敗時の処理
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        } else {
-            // ログイン成功時の処理
-            return "redirect:/attendanceList";
-        }
+	
+	@GetMapping
+	public String view(Model model,LoginForm form) {
+		
+		
+		return "Login";
+	
     }
-
-    @GetMapping("/attendanceList")
-    public String attendanceList(Model model) {
-        String sql = "SELECT * FROM ATTENDANCES";
-        List<Map<String, Object>> attendances = jdbcTemplate.queryForList(sql);
-        model.addAttribute("attendances", attendances);
-        return "attendancelist";
-    }
+	
+	@PostMapping
+	public void login(LoginForm form) {
+		System.out.println(form.toString());
+	}
+	
 }
