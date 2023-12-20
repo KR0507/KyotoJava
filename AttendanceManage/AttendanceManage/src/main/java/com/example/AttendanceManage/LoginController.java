d930c479f989daaf519adc1e842a0c74e04f22b1
Ryunosuke Konaka <k.ryunosuke257@gmail.com>
2023-12-20 11:20:39 +0900

    Login処理

--- AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
+++ AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
@@ -4,20 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.AttendanceManage.form.LoginForm;
 
+
 @Controller
-@RequestMapping("/Login")
 public class LoginController {
 	
-	@GetMapping
+	@GetMapping("/Login")
 	public String view(Model model,LoginForm form) {
 		
-		
 		return "Login";
-	
     }
 	
 	@PostMapping
