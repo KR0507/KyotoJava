b8885b1305eb8df63b13cf54946b73d4d3eae3c0
Ryunosuke Konaka <k.ryunosuke257@gmail.com>
2023-12-24 17:01:27 +0900

    javaなど

--- AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
+++ AttendanceManage/AttendanceManage/src/main/java/com/example/AttendanceManage/LoginController.java
@@ -11,15 +11,26 @@ import com.example.AttendanceManage.form.LoginForm;
 @Controller
 public class LoginController {
 	
+	private static final String LOGIN_ID = "user";
+	private static final String PASSWORD = "pwd";
+	
+	
 	@GetMapping("/Login")
 	public String view(Model model,LoginForm form) {
 		
 		return "Login";
     }
 	
-	@PostMapping
-	public void login(LoginForm form) {
-		System.out.println(form.toString());
+	@PostMapping("/Login")
+	public String login(Model model,LoginForm form) {
+		var isCorrectUserAuth = form.getLoginId().equals(LOGIN_ID)
+				&& form.getPassword().equals(PASSWORD);
+		if (isCorrectUserAuth) {
+			return "redirect:/Menu";
+		}else {
+			model.addAttribute("errorMsg","IDもしくはパスワードが間違いです。");
+			return "Login";
+		}
 	}
 	
 }
